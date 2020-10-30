bd5e4569851a0f98cf8d63f22860f65f17fd3cfa
Harley Hanna <harleyhannawrit@gmail.com>
2020-10-30 15:11:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,14 @@ class ApplicationController < Sinatra::Base
   end
   
   patch '/articles/:id' do
-    
+    id = params["id"]
+    new_params = {}
+    old_article = Article.find(id)
+    new_params[:title] = params["title"]
+    new_params[:content] = params["content"]
+    old_article.update(new_params)
+
+    redirect "/articles/#{id}"
   end 
   
   
