4c49afcfad7a16da041f63552c0b7710ce24f42a
Harley Hanna <harleyhannawrit@gmail.com>
2020-10-30 15:13:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,7 +51,7 @@ class ApplicationController < Sinatra::Base
   delete '/articles/:id/delete' do
     @article = Article.find(params[:id])
     @article.destroy
-    erb :delete
+    redirect '/articles'
   end 
   
   
